789dd3f4d7db13df282eb21c6ac06574f4777469
Deeksha Sridhar <102199843+Deeksha1703@users.noreply.github.com>
2023-10-03 01:43:15 +0800

    Update drawing_classifier.py

--- drawing_classifier.py
+++ drawing_classifier.py
@@ -20,6 +20,9 @@ from sklearn.linear_model import LogisticRegression
 class DrawingClassifierApp:
 
     def __init__(self):
+        """
+        Initializes an instance of the DrawingClassifierApp class.
+        """
         self.class1_name, self.class2_name, self.class3_name = None, None, None
         self.class1_counter, self.class2_counter, self.class3_counter = None, None, None
         self.classifier = None
@@ -37,6 +40,10 @@ class DrawingClassifierApp:
         self.init_gui()
 
     def prompt_project_details(self):
+        """
+        Prompts the user for project details, including project name and class names.
+        Creates directories for the project and classes if they do not exist.
+        """
         msg = Tk()
         msg.withdraw()
 
@@ -47,6 +54,10 @@ class DrawingClassifierApp:
             self.prompt_class_names(msg)
 
     def prompt_class_names(self, msg):
+        """
+        Prompts the user for class names and initializes counters and the classifier.
+        Creates directories for the classes.
+        """
         self.class1_name = simpledialog.askstring("Class 1", "What is the first class called?", parent=msg)
         self.class2_name = simpledialog.askstring("Class 2", "What is the second class called?", parent=msg)
         self.class3_name = simpledialog.askstring("Class 3", "What is the third class called?", parent=msg)
@@ -65,6 +76,9 @@ class DrawingClassifierApp:
         os.chdir("..")
 
     def load_data(self):
+        """
+        Loads project data from a pickle file.
+        """
         with open(f"{self.project_name}/{self.project_name}_data.pickle", "rb") as f:
             data = pickle.load(f)
         self.class1_name = data['class1_name']
@@ -77,6 +91,9 @@ class DrawingClassifierApp:
         self.project_name = data['project_name']
 
     def init_gui(self):
+        """
+        Initializes the GUI window and components.
+        """
         WIDTH = 500
         HEIGHT = 500
         WHITE = (255, 255, 255)
@@ -143,12 +160,32 @@ class DrawingClassifierApp:
         self.root.mainloop()
 
     def paint(self, event):
+        """
+        Handles the drawing of images on the canvas.
+
+        Parameters:
+            event (Tkinter event): The mouse event that triggered the drawing.
+
+        This function captures the mouse motion event and draws on the canvas
+        using the specified brush width.
+
+        """
         x1, y1 = (event.x - 1), (event.y - 1)
         x2, y2 = (event.x + 1), (event.y + 1)
         self.canvas.create_rectangle(x1, y1, x2, y2, fill="black", width=self.brush_width)
         self.draw.rectangle([x1, y2, x2 + self.brush_width, y2 + self.brush_width], fill="black", width=self.brush_width)
 
     def save(self, class_num):
+        """
+        Saves the current drawing to a specified class folder.
+
+        Parameters:
+            class_num (int): The class number to which the drawing belongs (1, 2, or 3).
+
+        This function saves the current drawing to the appropriate class folder
+        based on the class number.
+
+        """
         img = self._save_image()
         if class_num == 1:
             img.save(f"{self.project_name}/{self.class1_name}/{self.class1_counter}.png", "PNG")
@@ -163,17 +200,42 @@ class DrawingClassifierApp:
         self.clear_canvas()
 
     def brush_minus(self):
+        """
+        Decreases the brush width by 1.
+
+        This function decreases the brush width used for drawing by 1 unit.
+
+        """
         if self.brush_width > 1:
             self.brush_width -= 1
 
     def brush_plus(self):
+        """
+        Increases the brush width by 1.
+
+        This function increases the brush width used for drawing by 1 unit.
+
+        """
         self.brush_width += 1
 
     def clear_canvas(self):
+        """
+        Clears the drawing canvas.
+
+        This function clears the entire drawing canvas, removing all drawn elements.
+
+        """
         self.canvas.delete("all")
         self.draw.rectangle([0, 0, 1000, 1000], fill="white")
 
     def train_model(self):
+        """
+        Trains a machine learning model using the saved drawings.
+
+        This function collects image data from the saved drawings of different classes,
+        reshapes the data, and trains a machine learning classifier using the data.
+
+        """
         img_list = np.array([])
         class_list = np.array([])
 
@@ -201,6 +263,13 @@ class DrawingClassifierApp:
         tkinter.messagebox.showinfo("Drawing Classifier", "Model successfully trained!")
 
     def predict(self):
+        """
+        Predicts the class of a drawn image.
+
+        This function saves the current drawing as an image, loads the image, and uses
+        the trained classifier to predict the class label for the drawing.
+
+        """
         img = self._save_image()
         img.save("predict_shape.png", "PNG")
 
@@ -215,12 +284,32 @@ class DrawingClassifierApp:
             tkinter.messagebox.showinfo("Drawing Classifier", f"The drawing is probably a {self.class3_name}")
 
     def _save_image(self):
+        """
+        Saves and resizes an image.
+
+        This function saves the current image associated with the class instance as "temp.png",
+        opens it, resizes it to a thumbnail of size (50, 50) using antialiasing, and returns the resized image.
+
+        Returns:
+            PIL.Image.Image: The resized image.
+
+        """
         self.image.save("temp.png")
         result = PIL.Image.open("temp.png")
         result.thumbnail((50, 50), PIL.Image.ANTIALIAS)
         return result
 
     def change_model(self):
+        """
+        Changes the classifier model used in the class instance.
+
+        This function switches the classifier model from the current one to the next one
+        in a predefined sequence: LinearSVC -> KNeighborsClassifier -> LogisticRegression ->
+        DecisionTreeClassifier -> RandomForestClassifier -> GaussianNB -> LinearSVC.
+
+        Updates the status_label to display the name of the current model.
+
+        """
         if isinstance(self.classifier, LinearSVC):
             self.classifier = KNeighborsClassifier()
         elif isinstance(self.classifier, KNeighborsClassifier):
@@ -237,18 +326,40 @@ class DrawingClassifierApp:
         self.status_label.config(text=f"Current Model: {type(self.classifier).__name__}")
 
     def save_model(self):
+        """
+        Saves the current classifier model to a file.
+
+        This function opens a file dialog to let the user choose a destination file for
+        saving the current classifier model as a pickle file.
+
+        """
         file_path = filedialog.asksaveasfilename(defaultextension="pickle")
         with open(file_path, "wb") as f:
             pickle.dump(self.classifier, f)
         tkinter.messagebox.showinfo("Drawing Classifier", "Model successfully saved!")
 
     def load_model(self):
+        """
+        Loads a classifier model from a file.
+
+        This function opens a file dialog to let the user select a pickle file containing
+        a classifier model and loads it into the class instance.
+
+        """
         file_path = filedialog.askopenfilename()
         with open(file_path, "rb") as f:
             self.classifier = pickle.load(f)
         tkinter.messagebox.showinfo("Drawing Classifier", "Model successfully loaded!")
 
     def save_everything(self):
+        """
+        Saves various project-related data to a pickle file.
+
+        This function saves various project-related data, including class names, counters,
+        classifier model, and project name, to a pickle file. The file is named based on
+        the project name.
+
+        """
         data = {"class1_name": self.class1_name, "class2_name": self.class2_name, "class3_name": self.class3_name,
                 "class1_counter": self.class1_counter, "class2_counter": self.class2_counter,
                 "class3_counter": self.class3_counter, "classifier": self.classifier, "project_name": self.project_name}
@@ -257,6 +368,14 @@ class DrawingClassifierApp:
         tkinter.messagebox.showinfo("Drawing Classifier", "Project successfully saved!")
 
     def on_closing(self):
+        """
+        Handles the closing of the application window.
+
+        This function prompts the user with a dialog to confirm whether they want to save
+        their work before closing the application. If the user chooses to save, it calls
+        the `save_everything` function before destroying the application window.
+
+        """
         answer = tkinter.messagebox.askyesnocancel("Quit?", "Do you want to save your work?")
         if answer is not None:
             if answer:
